9a0d35fa8f93aaa5346f3b202fda00890f9f6ef6
PauloCalletti <pauloonlline@gmail.com>
2021-08-16 10:56:18 -0300

    Criando um FAQ simples

--- components/Faq.vue
+++ components/Faq.vue
@@ -1,24 +1,52 @@
 <template>
-   <div class="w-full">
-      <div class="py-5 text-4xl text-center font-bold">
-         <h1>
-            <span>Dúvidas Recorrentes</span>
-         </h1>
-      </div>
-      <div class=" flex flex-wrap px-10">
-         <div class="w-full px-6">
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo officiis voluptatibus dolore laudantium 
-               commodi enim neque perferendis! Delectus similique animi laudantium, eius ea accusamus blanditiis ipsam dolores 
-               eos atque nobis.
-            </p>
+   <div class="p-10 container mx-auto">
+      <div class="w-full bg-gray-200 py-10 px-10">
+         <div class="py-5 text-4xl text-center font-bold">
+            <h1>
+               <span>Dúvidas Recorrentes</span>
+            </h1>
          </div>
-         <div class="w-full px-6">
-            <p>
-               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo officiis voluptatibus dolore laudantium 
-               commodi enim neque perferendis! Delectus similique animi laudantium, eius ea accusamus blanditiis ipsam dolores 
-               eos atque nobis.
-            </p>
+         <div class="flex flex-wrap max-w-5xl mx-auto mb-6">
+            <div class="w-full md:w-1/2 px-3 mb-6">
+               <div class="w-full px-6">
+                  <p class="text-lg font-bold">Pergunta um</p>
+                  <p>
+                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo officiis voluptatibus dolore laudantium 
+                     commodi enim neque perferendis! Delectus similique animi laudantium, eius ea accusamus blanditiis ipsam dolores 
+                     eos atque nobis.
+                  </p>
+               </div>
+            </div>
+            <div class="w-full md:w-1/2 px-3 mb-6">
+               <div class="w-full px-6">
+                  <p class="text-lg font-bold">Pergunta dois</p>
+                  <p>
+                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo officiis voluptatibus dolore laudantium 
+                     commodi enim neque perferendis! Delectus similique animi laudantium, eius ea accusamus blanditiis ipsam dolores 
+                     eos atque nobis.
+                  </p>
+               </div>
+            </div>
+            <div class="w-full md:w-1/2 px-3 mb-6">
+               <div class="w-full px-6">
+                  <p class="text-lg font-bold">Pergunta tres</p>
+                  <p>
+                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo officiis voluptatibus dolore laudantium 
+                     commodi enim neque perferendis! Delectus similique animi laudantium, eius ea accusamus blanditiis ipsam dolores 
+                     eos atque nobis.
+                  </p>
+               </div>
+            </div>
+            <div class="w-full md:w-1/2 px-3 mb-6">
+               <div class="w-full px-6">
+                  <p class="text-lg font-bold">Pergunta quatro</p>
+                  <p>
+                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo officiis voluptatibus dolore laudantium 
+                     commodi enim neque perferendis! Delectus similique animi laudantium, eius ea accusamus blanditiis ipsam dolores 
+                     eos atque nobis.
+                  </p>
+               </div>
+            </div>
          </div>
       </div>
    </div>
