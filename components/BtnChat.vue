ce9a82e31049a630a23aaa771f4c9cdd587f9d0c
PauloCalletti <pauloonlline@gmail.com>
2021-08-19 11:41:05 -0300

    finalizando as vantagens e começando o contratar pacote

---
+++ components/BtnChat.vue
@@ -0,0 +1,9 @@
+<template>
+   <div class="p-12 bg-red-900">
+      <div class="">
+         <button class="border-8">
+            oi
+         </button>
+      </div>
+   </div>
+</template>
--- components/Vip.vue
+++ components/Vip.vue
@@ -1,6 +1,7 @@
 <template>
-   <div class="px-2 text-white 2xl:p-20 sm:bg-red-600">
-      <div class="grid grid-cols-3 ">
+   <div class=" grid grid-rows-3 px-2 text-white 2xl:p-20 ">
+      <!-- Preços -->
+      <div class="grid grid-cols-3">
          <!-- primeiro preço -->
          <div class=" w-full h-full grid justify-items-stretch">
             <div class="shadow-lg grid justify-items-stretch p-4">
@@ -40,27 +41,97 @@
          <!--  primeira coluna de promocines -->
          <div class="mt-12 justify-self-center ">
             <ul class="">
-               <li class="p-2">Desvantagem 1 </li> 
-               <li class="p-2">Desvantagem 2 </li> 
-               <li class="p-2">Desvantagem 3 </li> 
+               <!-- primeira vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div>
+               <!-- segunda vantagem-->  
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 2
+                  </li>             
+               </div>
+               <!-- terceira vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 3  
+                  </li>             
+               </div>
             </ul>
          </div>
-         <!-- segunda cuna de promociones-->
+         <!-- segunda coluna de promociones-->
          <div class="mt-12 justify-self-center ">
             <ul class="">
-               <li class="p-2">Desvantagem 1 </li> 
-               <li class="p-2">Desvantagem 2 </li> 
-               <li class="p-2">Desvantagem 3 </li> 
+               <!-- primeira vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div> 
+               <!-- segunda vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div>
+               <!-- terceira vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div>
             </ul>
          </div>
          <!-- terceira coluna de promciones-->
          <div class="mt-12 justify-self-center ">
             <ul class="">
-               <li class="p-2">Desvantagem 1 </li> 
-               <li class="p-2">Desvantagem 2 </li> 
-               <li class="p-2">Desvantagem 3 </li> 
+               <!-- primeira vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div> 
+               <!-- segunda vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div>
+               <!-- terceira vantagem-->
+               <div class="flex items-center ">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="text-green-500 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                  <li class="p-2 ">Desvantagem 1  
+                  </li>             
+               </div>
             </ul>
          </div>
       </div>
+      <!-- contratar pacotes-->
+      <div class="border-8">
+         <div>
+            <!-- contratar primeiro pacote-->
+         </div>
+      </div>
+   <BtnChat/>
    </div>
-</template>
+</template>  
