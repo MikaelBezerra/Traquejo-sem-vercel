8d2f708693c3c88ee1157d20016dd28dc46cf79c
PauloCalletti <pauloonlline@gmail.com>
2021-08-18 21:52:24 -0300

    Completando o front do carrousel

--- components/Contratar.vue
+++ components/Contratar.vue
@@ -1,10 +1,10 @@
 <template>
-   <div class="pt-20 px-20 grid grid-cols-2 h-screen w-auto ">
-      <div class="px-20">
+   <div class="pt-20 px-20 grid grid-cols-2 ">
+      <div class="">
          <Carrousel/>
-      </div>
-      <div class="px-20 ">
-         oi
+      </div >
+      <div class="">
+         <Vip/>
       </div>
    </div>
 </template>
