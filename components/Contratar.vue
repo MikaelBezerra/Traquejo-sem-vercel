bd13abe61663030c1eb5d3684559780b42886435
PauloCalletti <pauloonlline@gmail.com>
2021-08-18 23:03:17 -0300

    Começando a tebela de preços

--- components/Contratar.vue
+++ components/Contratar.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="pt-20 px-20 grid grid-cols-2 ">
+   <div class="pt-20 px-20 grid grid-cols-2 bg-gray-800">
       <div class="">
          <Carrousel/>
       </div >
---
+++ components/Vip.vue
@@ -0,0 +1,66 @@
+<template>
+   <div class="px-2 text-white 2xl:p-20 sm:bg-red-600">
+      <div class="grid grid-cols-3 ">
+         <!-- primeiro preço -->
+         <div class=" w-full h-full grid justify-items-stretch">
+            <div class="shadow-lg grid justify-items-stretch p-4">
+               <div class="justify-self-center">
+                  <p class="font-bold ">Texto de cima kkk lol</p>
+               </div>
+               <div class="justify-self-center">
+                  <p class="font-bold text-3xl">1 Zilion/sec</p>
+               </div>
+            </div>
+         </div>
+         <!-- segundo preço -->
+         <div class="w-full h-full grid justify-items-stretch">
+            <div class="shadow-lg grid justify-items-stretch p-4">
+               <div class="justify-self-center">
+                  <p class="font-bold ">Texto de cima kkk lol</p>
+               </div>
+               <div class="justify-self-center">
+                  <p class="font-bold text-3xl">1 Zilion/sec</p>
+               </div>
+            </div>
+         </div >
+         <!-- terceiro preço -->
+         <div class="w-full h-full grid justify-items-stretch">
+            <div class="shadow-lg grid justify-items-stretch p-4">
+               <div class="justify-self-center">
+                  <p class="font-bold ">Texto de cima kkk lol</p>
+               </div>
+               <div class="justify-self-center">
+                  <p class="font-bold text-3xl">1 Zilion/sec</p>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- conteudo de las promociones -->    
+      <div class="grid grid-cols-3 shadow-lg">
+         <!--  primeira coluna de promocines -->
+         <div class="mt-12 justify-self-center ">
+            <ul class="">
+               <li class="p-2">Desvantagem 1 </li> 
+               <li class="p-2">Desvantagem 2 </li> 
+               <li class="p-2">Desvantagem 3 </li> 
+            </ul>
+         </div>
+         <!-- segunda cuna de promociones-->
+         <div class="mt-12 justify-self-center ">
+            <ul class="">
+               <li class="p-2">Desvantagem 1 </li> 
+               <li class="p-2">Desvantagem 2 </li> 
+               <li class="p-2">Desvantagem 3 </li> 
+            </ul>
+         </div>
+         <!-- terceira coluna de promciones-->
+         <div class="mt-12 justify-self-center ">
+            <ul class="">
+               <li class="p-2">Desvantagem 1 </li> 
+               <li class="p-2">Desvantagem 2 </li> 
+               <li class="p-2">Desvantagem 3 </li> 
+            </ul>
+         </div>
+      </div>
+   </div>
+</template>
