ef2f60326a6f49a4e3804cf5b69e1ff58e63ef57
Paulo Henrique Calletti <pauloonlline@gmail.com>
2021-10-28 12:42:12 +0000

    equipe

--- pages/acesso.vue
+++ pages/acesso.vue
@@ -5,15 +5,3 @@
   </div>
 </template>
 
-<script>
-
-async function oi(){
-  try {
-    await this.$strapi.find('Users');
-  } catch (error) {
-    console.error(error);
-  }
-}
-export default oi()
-
-</script>
